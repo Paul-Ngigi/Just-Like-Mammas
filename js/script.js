6f30012953995b366b24fa76ab3d9ae34144ac90
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-06 20:44:53 +0300

    add the business logic to the script file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Business Logic
+function order(size,topping,crust){
+    this.Pizzasize = size;
+    this.Pizzatopping = topping;
+    this.Pizzacrust = crust;
+    this.address = []
+}
+function addresses(county,city,street){
+    this.county = county;
+    this.city = city;
+    this.street = street;
+}
