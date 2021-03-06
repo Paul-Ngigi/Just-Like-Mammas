abcd67859a224561a82159001c1e6d050d59c318
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-06 21:02:07 +0300

    enable the event to pick in value

--- js/script.js
+++ js/script.js
@@ -16,6 +16,23 @@ $(document).ready(function () {
     // User Interface Logic
     $("form#orderForm").submit(function(event){
         event.preventDefault();
+
+        let inputtedPizzaSize = $("select.size").val();
+        let inputtedPizzaTopping = $("select.topping").val();
+        let inputtedPizzaCrust = $("select.crust").val();
+
+        console.log("Pizza size " , inputtedPizzaSize);
+        console.log("Pizza topping " , inputtedPizzaTopping);
+        console.log("Pizza crust " , inputtedPizzaCrust);
+
+        let inputtedCounty = $("input#county").val();
+        let inputtedCity = $("input#city").val();
+        let inputtedStreet = $("input#street").val();
+
+        console.log("County " , inputtedCounty);
+        console.log("City " , inputtedCity);
+        console.log("Street " , inputtedStreet);
+
     });
 
 });
