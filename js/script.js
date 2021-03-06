eb0fdf124953b7b195e20e57dfcba59f722582a9
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-06 20:49:17 +0300

    add an event listener to the form

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
-// Business Logic
-function order(size,topping,crust){
-    this.Pizzasize = size;
-    this.Pizzatopping = topping;
-    this.Pizzacrust = crust;
-    this.address = []
-}
-function addresses(county,city,street){
-    this.county = county;
-    this.city = city;
-    this.street = street;
-}
-
-// User Interface Logic
-
+$(document).ready(function () {
     console.log("Content Loaded");
+    // Business Logic
+    function order(size, topping, crust) {
+        this.Pizzasize = size;
+        this.Pizzatopping = topping;
+        this.Pizzacrust = crust;
+        this.address = []
+    }
+    function addresses(county, city, street) {
+        this.county = county;
+        this.city = city;
+        this.street = street;
+    }
+
+    // User Interface Logic
+    $("form#orderForm").submit(function(event){
+        event.preventDefault();
+    });
+
 });
