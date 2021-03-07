5674eaa8b21c73a5fbe26b3cfafab8cf025661d5
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-07 09:06:39 +0300

    link the user interface logic to the business logic

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     console.log("Content Loaded");
     // Business Logic
-    function order(size, topping, crust) {
+    function Order(size, topping, crust) {
         this.Pizzasize = size;
         this.Pizzatopping = topping;
         this.Pizzacrust = crust;
         this.address = []
     }
-    function addresses(county, city, street) {
+    function Addresses(county, city, street) {
         this.county = county;
         this.city = city;
         this.street = street;
@@ -20,19 +20,26 @@ $(document).ready(function () {
         let inputtedPizzaSize = $("select.size").val();
         let inputtedPizzaTopping = $("select.topping").val();
         let inputtedPizzaCrust = $("select.crust").val();
-
-        console.log("Pizza size " , inputtedPizzaSize);
-        console.log("Pizza topping " , inputtedPizzaTopping);
-        console.log("Pizza crust " , inputtedPizzaCrust);
+        let newOrder = new Order(inputtedPizzaSize,inputtedPizzaTopping,inputtedPizzaCrust);
+        
+        // console.log("Pizza size " , inputtedPizzaSize);
+        // console.log("Pizza topping " , inputtedPizzaTopping);
+        // console.log("Pizza crust " , inputtedPizzaCrust);
 
         let inputtedCounty = $("input#county").val();
         let inputtedCity = $("input#city").val();
         let inputtedStreet = $("input#street").val();
+        let newAddress = new Addresses(inputtedCounty,inputtedCity,inputtedStreet);
+        newOrder.address.push(newAddress);
+        // console.log(newOrder);
 
-        console.log("County " , inputtedCounty);
-        console.log("City " , inputtedCity);
-        console.log("Street " , inputtedStreet);
+        // console.log("County " , inputtedCounty);
+        // console.log("City " , inputtedCity);
+        // console.log("Street " , inputtedStreet);
 
+        $("input#county").val("");
+        $("input#city").val("");
+        $("input#street").val("");
     });
 
 });
